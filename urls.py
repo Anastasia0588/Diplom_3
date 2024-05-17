84f2c8bcc1bfbfd8b7d90ecd50ac35576b34191e
Anastasiia Drozdovskaia <anastasia0588@gmail.com>
2024-05-18 00:08:17 +0300

    Добавлен эндпоинт для авторизации юзера

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ BASE_URL = 'https://stellarburgers.nomoreparties.site/'
 
 register_url = BASE_URL + 'api/auth/register'
 user_url = BASE_URL + 'api/auth/user'
+user_login = BASE_URL + 'api/auth/user/login'
 
 PROFILE_PAGE = "https://stellarburgers.nomoreparties.site/account/profile"
 FEED_PAGE = "https://stellarburgers.nomoreparties.site/feed"
